db0a23ba4b86092f52e2fb2e6629b8f2b64e2694
Christopher L. Kristiansen <109615680+Cataclisem@users.noreply.github.com>
2026-01-14 18:54:57 +0100

    Changed variable names

--- Main.py
+++ Main.py
@@ -20,9 +20,9 @@ password = os.getenv("emailPassword")
 sender_email = os.getenv("sender_email")
 reciver_email = os.getenv("reciver_email")
 #Prompts
-prompts = open("Prompts.txt", "r", encoding="utf-8")
+prompts = open("emailPrompts.txt", "r", encoding="utf-8")
 
-titles = open("Titler.txt", "r", encoding="utf-8")
+titles = open("emailTitles.txt", "r", encoding="utf-8")
 
 
 #Api key, plz dont share
@@ -34,4 +34,5 @@ client = Groq(api_key=GPT_api_key)
 ss_real = sendSpam(GPT_api_key, smtpServer, port, login, password, sender_email, reciver_email)
 
 ss_fake = sendSpam(GPT_api_key, "localhost", 1025, "", "", sender_email, reciver_email)
+
 ss_real.send_email_with_ai(prompts, titles)
